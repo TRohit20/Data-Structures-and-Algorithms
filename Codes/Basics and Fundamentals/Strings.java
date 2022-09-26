5735906d9137b4d02661a588fee837b9d9a62007
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-09-27 00:09:48 +0530

    Strings

---
+++ Codes/Basics and Fundamentals/Strings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        When two reference variables point to the same value or object
+        String name="Rohit";
+        String name2="Rohit";
+//        Checking if it's true
+//        You can check that in two ways:
+//        (I). Using the '==' comparator operator.
+        System.out.println(name == name2);
+//        (II). Using the .equals() method
+        System.out.println(name.equals(name2));
+//        Here's how you can create two objects of the same value
+        String a=new String("Rohit");
+        String b=new String("Rohit");
+//        By doing this, these objects while having the same value are created differently
+//        And are stored outside the string pool
+        System.out.println(a.equals(b));
+    }
+}
--- Codes/Strings.java
+++
@@ -1,9 +0,0 @@
-public class Strings {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String name="Rohit";
-        System.out.println("Enter a name");
-        String name2= in.nextLine();
-        System.out.println(name == name2);
-    }
-}
