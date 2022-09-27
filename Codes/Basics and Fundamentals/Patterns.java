7b443f36095c89ab8d71d257212c2a2ae492e8eb
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-09-27 23:38:19 +0530

    Patterns

--- Codes/Basics and Fundamentals/Patterns.java
+++ Codes/Basics and Fundamentals/Patterns.java
@@ -74,5 +74,15 @@ public class Patterns {
             System.out.println();
         }
     }
+    public static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
 
 }
