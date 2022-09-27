65573f157b375e3f702bd1e1184b491fd14f6e3f
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-09-27 23:34:07 +0530

    Patterns

--- Codes/Basics and Fundamentals/Patterns.java
+++ Codes/Basics and Fundamentals/Patterns.java
@@ -1,6 +1,6 @@
 public class Patterns {
     public static void main(String[] args) {
-        pattern31(5);
+        pattern30(5);
     }
     static void pattern31(int n) {
         int originalN = n;
@@ -15,4 +15,21 @@ public class Patterns {
     }
 
 
+    static void pattern30(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
 }
