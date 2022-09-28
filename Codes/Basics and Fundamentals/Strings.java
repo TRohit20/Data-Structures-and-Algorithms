02b2c5929f28061d4c156eb4c63679a44b551b25
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-09-28 14:30:22 +0530

    Strings

--- Codes/Basics and Fundamentals/Strings.java
+++ Codes/Basics and Fundamentals/Strings.java
@@ -18,10 +18,30 @@ public class Strings {
 //        By doing this, these objects while having the same value are created differently
 //        And are stored outside the string pool
         System.out.println(a.equals(b));
-        prettyprinting();
+//        prettyprinting();
+        Operators();
     }
     public static void prettyprinting(){
         float a = 432.23452f;
-        System.out.println("Formatted Number would be ");
+        double b= 234.23432;
+//        Way to pretty print or format the number
+        System.out.printf("Formatted Number would be: %.1f",a);
+//        Based on placing of placeholders values are shown.
+//        Math is a class in java in which value of pi is pre-defined or inbuilt so we are just accessing it
+        System.out.printf("Formatted numbers would be: %.3f and %.2f and %.1f",b,a,Math.PI);
+    }
+    public static void Operators(){
+//        + Operator
+//        char is given in single quote, adding them will give us integer
+        System.out.println('a'+'b');
+//        Adding two strings will lead to concatenation like below:
+        System.out.println("a"+"b");
+//        If we add a char with a integer: it gives us a integer:
+        System.out.println('a'+7);
+//        You can convert both the expression above and get a char result by casting:
+//        Syntax: (data_type to cast)(expression)
+        System.out.println((char)('a'+7));
+//        if we add a string with integer we get: string concatenated with a integer
+        System.out.println("a"+7);
     }
 }
