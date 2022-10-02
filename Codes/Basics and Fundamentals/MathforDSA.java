f647c00dc9599575c091824d5332aa510ff18f2d
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-02 23:42:00 +0530

    Maths for DSA

--- Codes/Basics and Fundamentals/MathforDSA.java
+++ Codes/Basics and Fundamentals/MathforDSA.java
@@ -5,10 +5,20 @@ public class MathforDSA {
         Scanner in = new Scanner(System.in);
         int n= in.nextInt();
         System.out.println(isOdd(n));
+        int[] nums={3,3,2,1,2,4,4};
+        System.out.println(unique(nums));
     }
 //  Q1.  Function to check if N is even or odd
     public static boolean isOdd(int n){
 //        Performing and operation with just 1
         return (n & 1) ==1;
+    }
+//    Q2. Function to get the only number that does not repeat twice in an array:
+    public static int unique(int[] nums){
+        int unique=0;
+        for(int i=0; i<nums.length;i++){
+            unique ^= nums[i];
+        }
+        return unique;
     }
 }
