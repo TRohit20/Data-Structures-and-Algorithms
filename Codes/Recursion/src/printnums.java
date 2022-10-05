4426b0fbf25ef5a44a2a49461e1f2983a5087848
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-05 18:53:07 +0530

    Recursion

--- Codes/Recursion/src/printnums.java
+++ Codes/Recursion/src/printnums.java
@@ -1,7 +1,18 @@
 public class printnums {
     public static void main(String[] args) {
-        printnto1(5);
-        print1ton(5);
+//        printnto1(5);
+//        print1ton(5);
+        printboth(5);
+    }
+//    Function to print from N to 1 then 1 to N
+    public static void printboth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printnto1(n-1);
+        print1ton(n-1);
+        System.out.println(n);
     }
 //    Function to return N to 1 numbers
     public static int printnto1(int n){
