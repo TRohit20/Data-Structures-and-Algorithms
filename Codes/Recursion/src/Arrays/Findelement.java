e4fcf5c321474cd277f813279d57db67694bde25
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-07 20:05:58 +0530

    Recursion on Arrays

--- Codes/Recursion/src/Arrays/Findelement.java
+++ Codes/Recursion/src/Arrays/Findelement.java
@@ -1,10 +1,14 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class Findelement {
     public static void main(String[] args) {
-        int[] arr={2,9,3,5,2};
-        System.out.println(find(arr,9,0));
+        int[] arr={2,9,3,5,2,9};
+        find1(arr,9,0);
+        System.out.println(list);
     }
+//    Function to find a element in a sorted array with no duplicates
     public static int find(int[] arr,int target,int index){
 //        Base condition
         if(index == arr.length){
@@ -15,5 +19,16 @@ public class Findelement {
         }else{
             return find(arr,target,index+1);
         }
+    }
+//    Function to find the element in an array with duplicates
+    static ArrayList<Integer> list= new ArrayList();
+    public static void find1(int[] arr,int target,int index){
+        if(index==arr.length){
+            return;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        find1(arr,target,index+1);
     }
 }
