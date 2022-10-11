e4f245ca2785396e6bd54b0635b676f3654a9d4b
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-11 23:12:31 +0530

    Bubble Sort

--- Codes/Recursion/src/Arrays/Sorting/BubbleSort.java
+++ Codes/Recursion/src/Arrays/Sorting/BubbleSort.java
@@ -1,27 +1,29 @@
 package Arrays.Sorting;
 
+
 import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr= {5,4,3,2,1,0};
+        int[] arr= {5,4,3,2,1};
+        System.out.println(Arrays.toString(bubblesort(arr,arr.length-1,0)));
     }
 //    Function to perform bubble sort using recursion
-    public static void bubblesort(int[] arr,int row, int col){
+    public static int[] bubblesort(int[] arr,int row, int col){
 //        Base condition
         if(row == 0){
-            return;
+            return arr;
         }
         if( col < row){
-            if(arr[col] < arr[col+1]){
+            if(arr[col] > arr[col+1]){
 //                Swap the elements
                 int temp = arr[col];
                 arr[col]=arr[col+1];
                 arr[col+1] = temp;
             }
             bubblesort(arr,row,col+1);
-        } else{
-            bubblesort(arr,row-1,0);
         }
+        return bubblesort(arr,row-1,0);
+
     }
 }
