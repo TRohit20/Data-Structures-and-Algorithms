531a2a72272da33d24ffbf5ca368b40ea7051030
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-15 13:32:37 +0530

    Assignment Questions

---
+++ Codes/Assignments/src/RunningSum.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class RunningSum {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+    public static int[] runningSum(int[] nums) {
+        int i = 1;
+        while(i < nums.length){
+            nums[i] = nums[i] + nums[i-1];
+            i++;
+        }
+        return nums;
+    }
+}
