f4d32983ecf657f1127d0b3a22ac5f672b01554f
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-02-08 18:28:49 +0530

    Constructor Overloading and this keyword better understanding.

--- Codes/OOPs/src/Main.java
+++ Codes/OOPs/src/Main.java
@@ -2,10 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Employee rohit; //You are only declaring the object not creating it
 
-        rohit = new Employee();
-
+        rohit = new Employee(14, "Rohit", 99.2f);
         rohit.greeting();
         rohit.changeName("Sneakerhead");
+
+        Employee karthik = new Employee();
+        System.out.println(karthik.employeeName);
+
+        Employee random = new Employee(karthik);
+        System.out.println(karthik.performance);
     }
 
 //    Creating a class
@@ -14,10 +19,25 @@ public class Main {
         String employeeName;
         float performance;
 
+//      Student xyz = new Student(17, "xyx", 89.7f) ;
+//      Here, this will be replaced with xyx
+        Employee(int id, String name, float perf){
+            this.employeeID = id;
+            this.employeeName = name;
+            this.performance = perf;
+        }
+
         Employee(){
             this.employeeID = 12;
-            this.employeeName = "Rohit";
-            this.performance= 99.2f;
+            this.performance = 99.5f;
+            this.employeeName = "Geek lover";
+        }
+
+//        Creating a constructor that takes value from another object
+        Employee(Employee instance){
+            this.employeeID = instance.employeeID;
+            this.employeeName = instance.employeeName;
+            this.performance = instance.performance;
         }
 
         void greeting(){
