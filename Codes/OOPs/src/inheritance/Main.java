7031f0b0fab77396d74917c353cba0254a603e35
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-02-11 16:17:46 +0530

    "Private" Keyword and References of classes

--- Codes/OOPs/src/inheritance/Main.java
+++ Codes/OOPs/src/inheritance/Main.java
@@ -7,5 +7,19 @@ public class Main {
 
         System.out.println(h1.length + " " + h1.squareFeet);
         System.out.println((h2.length + " "+ h2.squareFeet + " " + h2.rooms));
+
+        House h3 = new CustomHouse(10,143,12423,3);
+        System.out.println(h3.squareFeet);
+//        Here, It's the type of ref_var actually determines which mems can be accessed and not the type of object
+//        Since, 'rooms' is mem of child/subclass, it cannot access it
+//        System.out.println(h3.rooms);
+
+//        CustomHouse h4 = new House(12,14,1452);
+        // there are many variables in both parent and child classes
+        // you are given access to variables that are in the ref type i.e. CustomHouse
+        // Hence, you should have access to rooms variable
+        // this also means, that the ones you are trying to access should be initialised
+        // but here, when the obj itself is of type parent class, how will you call the constructor of a child class?
+//        The parent class does not know that 'rooms' exists, hence the error
     }
 }
