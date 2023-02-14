ee90b77e1babe4d4f1ae1fd3152e5338c321d085
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-02-14 15:11:15 +0530

    Object class methods

--- Codes/OOPs/src/accessControls/Main.java
+++ Codes/OOPs/src/accessControls/Main.java
@@ -5,5 +5,23 @@ public class Main {
         Practise obj = new Practise(10,"Rohit");
 
         obj.getName();
+
+        ObjectDemo obj1 = new ObjectDemo(12);
+        ObjectDemo obj2 = new ObjectDemo(13);
+
+        if(obj1 == obj2){
+            System.out.println("Equal");
+        }
+
+        if(obj1.equals(obj2)){
+            System.out.println("True ");
+        } else{
+            System.out.println("False");
+        }
+
+//        Instanceof Method
+        System.out.println(obj instanceof Practise);
+//        Getclass Method
+        System.out.println(obj.getClass().getName());
     }
 }
--- Codes/OOPs/src/accessControls/ObjectDemo.java
+++ Codes/OOPs/src/accessControls/ObjectDemo.java
@@ -1,5 +1,33 @@
 package accessControls;
 
 public class ObjectDemo {
+    int num;
+
+    ObjectDemo(int num){
+        this.num = num;
+    }
+
+//    Basically gives the number representation of an object
+//    A unique representation of a object via number
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+//    Already covered
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 
 }
