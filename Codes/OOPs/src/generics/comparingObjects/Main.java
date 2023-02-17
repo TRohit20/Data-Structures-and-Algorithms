50769de85400c4d1c7c3b687081cf8e59ae130ea
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-02-17 23:56:35 +0530

    Comparing Objects using Comparable Interface

--- Codes/OOPs/src/generics/comparingObjects/Main.java
+++ Codes/OOPs/src/generics/comparingObjects/Main.java
@@ -6,11 +6,11 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        Student rohit = new Student(99.7f,57);
-        Student santosh = new Student(99.5f,01);
-        Student giridhar = new Student(99.3f,02);
-        Student lakshana = new Student(99.2f,03);
-        Student keerthi = new Student(99.1f,04);
+        Student rohit = new Student(57,99.7f);
+        Student santosh = new Student(01,99.5f);
+        Student giridhar = new Student(02,99.3f);
+        Student lakshana = new Student(03,99.2f);
+        Student keerthi = new Student(04,99.1f);
 
         Student[] students = {santosh, rohit, keerthi, lakshana, giridhar};
         System.out.println(Arrays.toString(students));
@@ -19,8 +19,8 @@ public class Main {
         System.out.println(Arrays.toString(students));
 
 
-//        if(rohit.compareTo(santosh) < 0 ){
-//            System.out.println("Santosh has more marks than rohit");
-//        }
+        if(rohit.compareTo(santosh) < 0 ){
+            System.out.println("Santosh has more marks than rohit");
+        }
     }
 }
--- Codes/OOPs/src/generics/comparingObjects/Student.java
+++ Codes/OOPs/src/generics/comparingObjects/Student.java
@@ -1,22 +1,26 @@
 package generics.comparingObjects;
 
 public class Student implements Comparable<Student>{
-    float marks;
     int rollNumber;
+    float marks;
 
-    Student(float marks, int rollno){
+    Student(int rollno, float marks){
         this.marks = marks;
         this.rollNumber = rollno;
     }
 
     @Override
     public int compareTo(Student o) {
-//        If the value is equal to 0, then both are equal.
-//        if Diff is < 1 means name is bigger else its smaller
+        System.out.println("in compare to method");
         int diff = (int)(this.marks - o.marks);
+
+        // if diff == 0: means both are equal
+        // if diff < 0: means o is bigger else o is smaller
+
         return diff;
     }
 
+    @Override
     public String toString(){
         return marks + " ";
     }
